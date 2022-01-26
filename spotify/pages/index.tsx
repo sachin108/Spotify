ed82c85eac0d3d5d003df05260c5e32b0add00af
sachin108 <sachin108rajput@hotmail.com>
2022-01-26 19:52:56 +0530

    updated

---
+++ spotify/pages/index.tsx
@@ -0,0 +1,13 @@
+import Sidebar from '../components/Sidebar';
+
+export default function Home() {
+  return (
+    <div className=' bg-black h-screen overflow-hidden'>
+      <main>
+        <Sidebar/>
+        {/**centre*/}
+      </main>
+      <div>{/**Player */}</div>
+    </div>
+  )
+}
